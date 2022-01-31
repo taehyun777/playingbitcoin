8b8df10321c010c7dcad471312418d33f5bce44c
taehyun777 <98208100+taehyun777@users.noreply.github.com>
2022-01-31 12:20:33 +0900

    Update taehyun5.py

--- taehyun5.py
+++ taehyun5.py
@@ -58,34 +58,7 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-BTC")
         end_time = start_time + datetime.timedelta(days=1)
-
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            ma15 = get_ma15("KRW-BTC")
-            current_price = get_current_price("KRW-BTC")
-            if target_price < current_price and ma15 < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
-                    post_message(myToken,"#crypto", "BTC buy : " +str(buy_result))
-        else:
-            btc = get_balance("BTC")
-            if btc > 0.00008:
-                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
-                post_message(myToken,"#crypto", "BTC buy : " +str(sell_result)),
-
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-ETH", 0.5)
-            current_price = get_current_price("KRW-ETH")
-            if target_price < current_price:
-                krw = get_balance("ETH")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-ETH", krw*0.9995)
-        else:
-            btc = get_balance("ETH")
-            if btc > 0.00008:
-                upbit.sell_market_order("KRW-ETH", btc*0.9995),
-
+    
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-SAND", 0.5)
             current_price = get_current_price("KRW-SAND")
